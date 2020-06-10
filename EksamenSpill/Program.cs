78e60227f1aeabe8b7b91b070a15aca81fdf28f1
natnaelasgedom <natnael.asgedom@gmail.com>
2020-06-10 19:03:09 +0200

    adding interface IBrukergrensesnitt

---
+++ EksamenSpill/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EksamenSpill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public interface IBrukergrensesnitt
+    {
+        void giStatus(string status);
+        int beOmKommando(string spørsmål, string[] alternativet);
+    }
+}
