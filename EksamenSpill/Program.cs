b34f622559c61f16951dad6ad2a52045414d6030
LisaFosH <lisafosse@gmail.com>
2020-06-11 11:42:34 +0200

    Skrev Lisa

--- EksamenSpill/Program.cs
+++ EksamenSpill/Program.cs
@@ -6,7 +6,7 @@ namespace EksamenSpill
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Lisa");
         }
     }
 
