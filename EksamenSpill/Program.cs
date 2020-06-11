fb6f695772d4ff9b2cad26e59e3f0f8d0e74634d
kkilli <kristian.killi@hotmail.com>
2020-06-11 14:04:27 +0200

    cw

--- EksamenSpill/Program.cs
+++ EksamenSpill/Program.cs
@@ -10,6 +10,7 @@ namespace EksamenSpill
 			Console.WriteLine("HEEELZZ YEAH!!!!");
             Console.WriteLine("Hello, world!");
             Console.WriteLine("Grevling");
+            Console.WriteLine("");
         }
     }
 
