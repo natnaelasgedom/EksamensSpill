08225ff965ab9f97a76bf324ff72b4d0d44c80d7
natnaelasgedom <natnael.asgedom@gmail.com>
2020-06-12 16:45:30 +0200

    Written out Robot_BeOmKommando()

--- EksamenSpill/Program.cs
+++ EksamenSpill/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace EksamenSpill
 {
@@ -7,6 +8,33 @@ namespace EksamenSpill
         static void Main(string[] args)
         {
            
+        }
+    }
+
+    public class Robot : IBrukergrensesnitt
+    {
+        //StreamReader streamReader;
+
+        //public Terminal(StreamReader streamReader)
+        //{
+        //    this.streamReader = streamReader;
+        //}
+        public void GiStatus(string status)
+        {
+            Console.WriteLine(">>>Status: " + status);
+        }
+
+        public int BeOmKommando(string spørsmål, string[] alternativer)
+        {
+            Console.WriteLine(spørsmål);
+            for (int i = 1; i <= alternativer.Length; i++)
+            {
+                Console.WriteLine(i + ": " + $"{alternativer[i - 1]}");
+            }
+
+            int robotValg = new Random().Next(1, alternativer.Length+1);
+
+            return robotValg;
         }
     }
 }
